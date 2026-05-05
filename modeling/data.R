001de822f64182dc36c05a197078e572fad5e576
Chris <32319369+cgiven25@users.noreply.github.com>
2026-05-05 00:15:54 -0400

    reorganized and did a bit more analysis

---
+++ modeling/data.R
@@ -0,0 +1,111 @@
+cap <- tibble(read.csv("cap_data_sportrac.csv")) %>%
+  mutate(team = case_match(team,
+                           "WAS" ~ "WSH",
+                           "VGK" ~ "VEG",
+                           "ARI" ~ "UTA",
+                           .default = team)
+  )
+
+leagues <- tibble(read.csv("league.csv"))
+leagues$name <- c(
+  "Buffalo Sabres", "Tampa Bay Lightning", "Montreal Canadiens", "Boston Bruins",
+  "Ottawa Senators", "Detroit Red Wings", "Florida Panthers", "Toronto Maple Leafs",
+  "Carolina Hurricanes", "Pittsburgh Penguins", "Philadelphia Flyers", "Washington Capitals",
+  "Columbus Blue Jackets", "New York Islanders", "New Jersey Devils", "New York Rangers",
+  "Colorado Avalanche", "Dallas Stars", "Minnesota Wild", "Utah Mammoth",
+  "St. Louis Blues", "Nashville Predators", "Winnipeg Jets", "Chicago Blackhawks",
+  "Vegas Golden Knights", "Edmonton Oilers", "Anaheim Ducks", "Los Angeles Kings",
+  "San Jose Sharks", "Seattle Kraken", "Calgary Flames", "Vancouver Canucks", "Arizona Coyotes"
+)
+leagues <- leagues %>%
+  relocate(name) %>%
+  rename(code = team)
+
+
+seasons <- c("2021_2022", "2022_2023", "2023_2024", "2024_2025", "2025_2026")
+
+# okay. like. the utah/arizona thing is causing missing values in cap data
+# cause my renaming happens before that join
+# but it's just 3 rows (the three observations before ARI changed to UTA) so i'm ignoring it for now
+  # yeah this is fine they suck so we dont have to worry about them too much lol
+# TODO: fix it
+full_data <- data.frame()
+for (s in seasons) {
+  season_data <- tibble(read.csv(paste0("season", s, ".csv"), header = T, skip =1)) %>%
+    rename(name = if(s == "2024_2025") "X." else "X") %>%
+    filter(name != "League Average") %>%
+    mutate(
+      name = case_match(name, c("Utah Hockey Club", "Arizona Coyotes") ~ "Utah Mammoth", .default = name),
+      MadePlayoffs = ifelse(grepl("\\*", name), 1, 0),
+      name = sub("\\*", "", name),
+      season = s
+    ) %>%
+    left_join(leagues, by = "name") %>%
+    relocate(c(code, division, conference), .after=name) %>%
+    left_join(subset(cap[cap$season == s,], select = -c(season)), by = join_by(code == team))
+
+  full_data <- bind_rows(full_data, season_data)
+}
+
+reg_ot <- read.csv("reg_ot.csv") %>%
+  mutate(season = ifelse(season=="2022_2021", "2021_2022", season))
+full_data_with_overtimes <- full_data %>%
+  full_join(reg_ot, by = c("name", "season")) %>%
+  na.omit()
+
+full_data$W.binned <- cut(full_data$W, 4)
+
+train <- full_data %>%
+  filter(season %in% seasons[1:4])
+
+test <- full_data %>%
+  filter(season == seasons[5])
+
+fit <- glm(MadePlayoffs ~ AvAge + SOS + cap_space + ltir, family = "binomial", data = train)
+
+# does not look like it does a good job, but we have few predictors in the current model.
+# what makes sense, supposing we want to calculate the odds of making playoffs before games are played?
+predicted <- predict(fit, newdata = test, type = "response")
+plot(abs(predicted - test$MadePlayoffs))
+
+# honestly does not look that much worse than logistic
+fit.linear <- lm(MadePlayoffs ~ AvAge + SOS + division + cap_space, data = train)
+predicted.linear <- predict(fit.linear, newdata = test)
+points(abs(predicted.linear - test$MadePlayoffs), col = "red")
+sum(abs(predicted.linear - test$MadePlayoffs) < abs(predicted - test$MadePlayoffs)) # 13. In 13/32 cases, multiple linear regression is performing better
+
+# TODO: ordinal regression
+
+
+# things to also do: predict if playoffs based on SOS, if playoffs last year, division?
+# maybe we can also rank division 1 through 4 based on accumulated points or something..?
+# also want to do that 3-2-1 W-OTW-SOW thing but they dont count the number of overtime games????
+# wait its on a different table my bad everyone
+# ill get that in one sec
+
+# predict points
+fit2 <- glm(PTS ~ AvAge + SOS + division + cap_space, family = "poisson", data=train)
+summary(fit2)
+# can we think abt comparing multiple models year by year predicting making playoffs (when this data is current year)
+# ploffs ~ AvAge + SOS + division + cap_space
+
+fit2122 <- glm(MadePlayoffs ~ AvAge + SOS + division + cap_space, family = "binomial", data=filter(full_data, season=="2021_2022"))
+summary(fit2122)
+#okay this sucks
+# what if we did madePlayoffs last year or something?
+fit2223 <- glm(MadePlayoffs ~ SOS + cap_space, family = "binomial", data=filter(full_data, season=="2022_2023"))
+summary(fit2223)
+
+# 
+fit.points <- lm(PTS ~ AvAge + SOS + cap_space, data = train)
+plot(predict(fit.points), train[complete.cases(train), "PTS"])
+abline(0, 1)
+
+# we tried it yay
+div.from.pts <- multinom(division ~ PTS, data = train)
+
+# these plus PP and PK?
+t <- multinom(W.bin ~ AvAge + SOS + cap_space + ltir, data = train)
+
+# ordinal logistic regression: future work
+t2 <- polr(W.bin ~ AvAge + SOS + division, data = train)
--- modeling/models.R
+++ modeling/models.R
@@ -1,108 +1,128 @@
-cap <- tibble(read.csv("cap_data_sportrac.csv")) %>%
-  mutate(team = case_match(team,
-                           "WAS" ~ "WSH",
-                           "VGK" ~ "VEG",
-                           .default = team)
-  )
+# 2025-2026 season
+test <- readRDS("test.RDS")
 
-leagues <- tibble(read.csv("league.csv"))
-leagues$name <- c(
-  "Buffalo Sabres", "Tampa Bay Lightning", "Montreal Canadiens", "Boston Bruins",
-  "Ottawa Senators", "Detroit Red Wings", "Florida Panthers", "Toronto Maple Leafs",
-  "Carolina Hurricanes", "Pittsburgh Penguins", "Philadelphia Flyers", "Washington Capitals",
-  "Columbus Blue Jackets", "New York Islanders", "New Jersey Devils", "New York Rangers",
-  "Colorado Avalanche", "Dallas Stars", "Minnesota Wild", "Utah Mammoth",
-  "St. Louis Blues", "Nashville Predators", "Winnipeg Jets", "Chicago Blackhawks",
-  "Vegas Golden Knights", "Edmonton Oilers", "Anaheim Ducks", "Los Angeles Kings",
-  "San Jose Sharks", "Seattle Kraken", "Calgary Flames", "Vancouver Canucks", "Arizona Coyotes"
-)
-leagues <- leagues %>%
-  relocate(name) %>%
-  rename(code = team)
+# 2021-2025 seasons
+train <- readRDS("train.RDS")
+
+full <- rbind(train, test)
+# TODO: get range of all predictors to help with interpretation
 
 
-seasons <- c("2021_2022", "2022_2023", "2023_2024", "2024_2025", "2025_2026")
-
-# okay. like. the utah/arizona thing is causing missing values in cap data
-# cause my renaming happens before that join
-# but it's just 3 rows (the three observations before ARI changed to UTA) so i'm ignoring it for now
-  # yeah this is fine they suck so we dont have to worry about them too much lol
-# TODO: fix it
-full_data <- data.frame()
-for (s in seasons) {
-  season_data <- tibble(read.csv(paste0("season", s, ".csv"), header = T, skip =1)) %>%
-    rename(name = if(s == "2024_2025") "X." else "X") %>%
-    filter(name != "League Average") %>%
-    mutate(
-      name = case_match(name, c("Utah Hockey Club", "Arizona Coyotes") ~ "Utah Mammoth", .default = name),
-      MadePlayoffs = ifelse(grepl("\\*", name), 1, 0),
-      name = sub("\\*", "", name),
-      season = s
-    ) %>%
-    left_join(leagues, by = "name") %>%
-    relocate(c(code, division, conference), .after=name) %>%
-    left_join(subset(cap[cap$season == s,], select = -c(season)), by = join_by(code == team))
-
-  full_data <- bind_rows(full_data, season_data)
+# -------------------------------
+library(pROC)
+showROC <- function(model, response) {
+  roc(test[, response], predict(model, newdata = test, type="response"), smooth = T, plot = T)
 }
 
-reg_ot <- read.csv("reg_ot.csv") %>%
-  mutate(season = ifelse(season=="2022_2021", "2021_2022", season))
-full_data_with_overtimes <- full_data %>%
-  full_join(reg_ot, by = c("name", "season")) %>%
-  na.omit() %>%
-  mutate(OTW = W - RW - SOW,
-         newpts = 3*RW + 2*OTW + 1*OL + 2*SOW + 1*SOL)
+# TODO:
+# overtime stuff (ask Emma), currently not included in training/testing data
+
+# initial model (wanted to choose predictors we could know before season starts)
+# AvAge: higher average age performs worse? or better due to experience?
+# SOS: how difficult the schedule is for the team. measure of goals above (+) or below (-) average.
+log1 <- glm(MadePlayoffs ~ AvAge + SOS, data = train, family = "binomial")
+summary(log1) # sig: SOS
+showROC(log1, "MadePlayoffs") # area .672, not great
+
+# maybe we can get more information.
+# cap space: money left in a team's player salary budget
+# ltir: long term injury reserve - salary currently tied up by injured players. lets teams surpass the salary cap by that amount
+#  - requires them to predict that the player will miss >= 10 NHL games over 24 days of the season
+
+# Expecations:
+# cap space to be negatively correlated with making playoffs: their players not worth as much?
+# ltir: unsure? higher means that more players are out but it lets them spend money on new players. depends who is out, probably
+# division: certain divisions are just stronger.
+log2 <- glm(MadePlayoffs ~ AvAge + SOS + division + cap_space + ltir, data = train, family = "binomial")
+summary(log2) # sig: SOS, pacific, cap_space
+showROC(log2, "MadePlayoffs") # better, area .744
+
+# still not fantastic. with a cutoff of 0.5 (up to .8 same results):
+pred.made <- predict(log2, newdata = test, type="response") > .5
+sum(pred.made == test$MadePlayoffs) # 22/32 predicted correctly
+# only predicted that 10 teams would make the playoffs for cutoffs in [.5, .8]
+# 8 true positives, 2 false positives
+# 14 true negatives, 10 (!!) false negatives
+
+# these models are trash. why?
+# many features overlap for teams that make the playoffs vs teams that don't. hard to discern
+# machine learning approach maybe better? future work
+
+# what if we try to predict points? once point rankings are figured out, playoffs are determined
+loglin <- glm(PTS ~ AvAge + SOS + division + cap_space + ltir, data = train, family = "poisson")
+summary(loglin)
+predict.pts <- predict(loglin, newdata = test, type="response")
+order <- as.integer(names(sort(predict.pts, decreasing=T)))
+sorted.pts <- test[order,c("name", "division", "conference", "MadePlayoffs")]
+
+# --- justification detour ---
+
+# why poisson?
+hist(train$PTS, prob=T)
+hist(rnorm(1000, mean=mean(train$PTS), sd=sqrt(var(train$PTS))), prob=T, add=T, col=rgb(1, 0, 0, .3))
+hist(rpois(1000, mean(train$PTS)), prob=T, add=T, col=rgb(0, 1, 0, .3))
+# whoops this is bad don't show this lol
+
+# our response is a nonnegative integer, so a poisson model makes some sense?
+# i remembered negative binomial models: according to google they can help with overdispersion (and our data is overdispersed)
+nb <- glm.nb(PTS ~ AvAge + SOS + division + cap_space + ltir, data = train)
+# the Poisson model predictions are very similar. will be good to mention in the slides, though.
+# there are other options but we probably don't need to keep going with that
+
+# --- back to scheduled programming ---
+
+# top 3 teams each division, then the next two highest in the conference
+sorted.pts[sorted.pts$division == "atlantic",] # Atlantic: Lightning, Senators, Sabres
+sorted.pts[sorted.pts$division == "metropolitan",] # Metro: Hurricanes, Capitals, Islanders
+sorted.pts[sorted.pts$division == "central",] # Central: Avalanche, Stars, Wild
+sorted.pts[sorted.pts$division == "pacific",] # Pacific: Oilers, Knights, Kings
+sorted.pts[sorted.pts$conference == "eastern",] # Eastern Wildcards: Bruins, Panthers
+sorted.pts[sorted.pts$conference == "western",] # Western Wildcards: Mammoth, Ducks
+
+# real playoffs:
+# Eastern Conference:
+#   Atlantic: Sabres, Lightning, Canadiens
+#   Metropolitan: Hurricanes, Penguins, Flyers
+#   Wildcards: Bruins, Senators
+# Western Conference:
+#   Central: Avalanche, Stars, Wild
+#   Pacific: Knights, Oilers, Ducks
+#   Wildcards: Mammoth, Kings
+
+# performance (ignoring wildcard mismatch, just whether they really made playoffs or not)
+# Eastern Conference:
+#   correctly predicted 5 of 8 teams.
+# Western Conference:
+#   correctly predicted 8 of 8 teams (and mostly in order!)
 
 
-train <- full_data %>%
-  filter(season %in% seasons[1:4])
+# what if we bin wins and then try to predict the bin?
+# this is harder to convert into whether or not they made playoffs, but:
+xtabs(~ MadePlayoffs + W.binned, data = train)
+# 0/19 teams in the lowest bin made the playoffs, 6/49 in the second-lowest, 51/53 in the second highest, and 7/7 in the highest.
+# predicting the top two bins correctly would already be good
+library(nnet)
+multinom.W <- multinom(W.binned ~ AvAge + SOS + division +  cap_space + ltir, data = train)
+summary(multinom.W)
+pred.wbin <- predict(multinom.W, newdata = test)
+test.by.w <- cbind(test[,c("name", "W.binned", "MadePlayoffs")], predicted.bin = pred.wbin)
+View(test.by.w)
+# not great. no predicted teams in the highest bucket. buckets are bad, maybe?
 
-test <- full_data %>%
-  filter(season == seasons[5])
+# make buckets have equal number of observations
+test.adj <- test %>%
+  mutate(W.binned.adj = ntile(W, 4))
+train.adj <- train %>%
+  mutate(W.binned.adj = ntile(W, 4))
 
-fit <- glm(MadePlayoffs ~ AvAge + SOS + division + cap_space, family = "binomial", data = train)
+multinom.W.adj <- multinom(W.binned.adj ~ AvAge + SOS + division +  cap_space + ltir, data = train.adj)
+summary(multinom.W.adj)
+cbind(w.bin = test.adj$W.binned.adj, 
+      predicted.bin = predict(multinom.W.adj, newdata = test), 
+      playoffs = test.adj$MadePlayoffs)
+# just by visual inspection, this sucked. worth mentioning but not including
+# i'm guessing that the point values are so closely clustered that the buckets are too wide when basing them on the range of the data, or too small when breaking them into equal-sized groups.
+# in the first case, we limit our training data for certain bins. in the second, the groups become too hard to discern
 
-# does not look like it does a good job, but we have few predictors in the current model.
-# what makes sense, supposing we want to calculate the odds of making playoffs before games are played?
-predicted <- predict(fit, newdata = test, type = "response")
-plot(abs(predicted - test$MadePlayoffs))
-
-# honestly does not look that much worse than logistic
-fit.linear <- lm(MadePlayoffs ~ AvAge + SOS + division + cap_space, data = train)
-predicted.linear <- predict(fit.linear, newdata = test)
-points(abs(predicted.linear - test$MadePlayoffs), col = "red")
-sum(abs(predicted.linear - test$MadePlayoffs) < abs(predicted - test$MadePlayoffs)) # 13. In 13/32 cases, multiple linear regression is performing better
-
-# TODO: ordinal regression
-
-
-# things to also do: predict if playoffs based on SOS, if playoffs last year, division?
-# maybe we can also rank division 1 through 4 based on accumulated points or something..?
-# also want to do that 3-2-1 W-OTW-SOW thing but they dont count the number of overtime games????
-# wait its on a different table my bad everyone
-# ill get that in one sec
-
-# predict points
-fit2 <- glm(PTS ~ AvAge + SOS + division + cap_space, family = "poisson", data=train)
-summary(fit2)
-# can we think abt comparing multiple models year by year predicting making playoffs (when this data is current year)
-# ploffs ~ AvAge + SOS + division + cap_space
-fit3 <- glm(newpts ~ AvAge + SOS + division + cap_space, family = "poisson", data=full_data_with_overtimes)
-summary(fit3) #like okay
-fit4 <- glm(MadePlayoffs ~ OTW, family = "binomial", data=full_data_with_overtimes)
-summary(fit4) # interesting otw odds ratio 1.309964
-
-fit2122 <- glm(MadePlayoffs ~ AvAge + SOS + division + cap_space, family = "binomial", data=filter(full_data, season=="2021_2022"))
-summary(fit2122)
-#okay this sucks
-# what if we did madePlayoffs last year or something?
-fit2223 <- glm(MadePlayoffs ~ SOS + cap_space, family = "binomial", data=filter(full_data, season=="2022_2023"))
-summary(fit2223)
-# okay so this is still bad
-# i do think we need to explore last season (immediate last season) behavior
-# maybe i will try to wrangle coaching changes
-# the nhl is the worst league on the planet. there is no organized information for this
-fit2334
-fit2425
-fit2526
+# seems to me like our best model is the points loglinear model where we convert to playoffs by hand.
+# hard to tell without more data. 
